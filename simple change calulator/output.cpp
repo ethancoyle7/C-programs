99a38645cfcb3e7ab4aa9dd1a3afacfd902ed08e
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-01-24 15:39:49 -0600

    Create output.cpp

---
+++ simple change calulator/output.cpp
@@ -0,0 +1,101 @@
+ Ethan Coyle Program 2: Creating Change
+
+ Total Ammount is $   2.00
+        Dollar Bills           2
+        Half Dollars           0
+        Quarters               0
+        Dimes                  0
+        Nickles                0
+        Pennies                0
+
+
+ Total Ammount is $   39.29
+        Dollar Bills          39
+        Half Dollars           0
+        Quarters               1
+        Dimes                  0
+        Nickles                0
+        Pennies                4
+
+
+ Total Ammount is $   5.03
+        Dollar Bills           5
+        Half Dollars           0
+        Quarters               0
+        Dimes                  0
+        Nickles                0
+        Pennies                3
+
+
+ Total Ammount is $   100.91
+        Dollar Bills         100
+        Half Dollars           1
+        Quarters               1
+        Dimes                  1
+        Nickles                1
+        Pennies                1
+
+
+ Total Ammount is $   78.82
+        Dollar Bills          78
+        Half Dollars           1
+        Quarters               1
+        Dimes                  0
+        Nickles                1
+        Pennies                2
+
+
+ Total Ammount is $   0.55
+        Dollar Bills           0
+        Half Dollars           1
+        Quarters               0
+        Dimes                  0
+        Nickles                1
+        Pennies                0
+
+
+ Total Ammount is $   10.50
+        Dollar Bills          10
+        Half Dollars           1
+        Quarters               0
+        Dimes                  0
+        Nickles                0
+        Pennies                0
+
+
+ Total Ammount is $   3491.42
+        Dollar Bills        3491
+        Half Dollars           0
+        Quarters               1
+        Dimes                  1
+        Nickles                1
+        Pennies                2
+
+
+ Total Ammount is $   45.88
+        Dollar Bills          45
+        Half Dollars           1
+        Quarters               1
+        Dimes                  1
+        Nickles                0
+        Pennies                3
+
+
+ Total Ammount is $   87.09
+        Dollar Bills          87
+        Half Dollars           0
+        Quarters               0
+        Dimes                  0
+        Nickles                1
+        Pennies                4
+
+
+ Total Ammount is $   0.07
+        Dollar Bills           0
+        Half Dollars           0
+        Quarters               0
+        Dimes                  0
+        Nickles                1
+        Pennies                2
+
+
