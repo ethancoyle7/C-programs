2748c8f696e2f89f73def4a5c5107c32c551fdc7
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-02-23 15:10:42 -0600

    Update main.cpp

--- overloading program/main.cpp
+++ overloading program/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+
+
+
+ //(25 points)  Write aPoint3Dclassdefinitionthat will represent a 3D point.  Assume all values tobe integers.  Do not add any setters or getters.Include:
+//(a)  (5 points)  Include a default constructor that sets each data member to zero.
+//(b)  (10 points)  Include an overloaded constructor to init each data member.
+//(c)  (10 points)  Include a copy constructor.
+
+// class called point3d
+
 class Point3D
 {
+  // all values assigned to integer values for return
   private:
     int x;
     int y;
@@ -12,12 +23,14 @@ class Point3D
 public:
 
     // default Constructor here
+    // initialize all values to 0 for all three points
     Point3D()
     { 
-        x = y = z = 0;         // initializing all to 0
+        x = y = z = 0;         
     }
     
      // Overloaded Constructor that init each data here.
+     // initialize all 
     Point3D(int _x , int _y , int _z)
     {                 
         x = _x;
@@ -26,6 +39,7 @@ public:
     } 
     
     // Copy Constructor here.
+    // this will copy everything from one to new object
     Point3D(const Point3D &rhs)
     {
         this->x = rhs.x;
@@ -34,12 +48,22 @@ public:
     }
 
     // Overload Ostream for 3D class so : [x,y,z] here.
+    // Overloadostreamfor our 3D class so it prints the values like so: 
+    //[x, y, z] where x,y, and z would be integers (obviously)
+  
     friend ostream& operator<<(ostream &os,const Point3D &rhs)
     {
         return os << "[" << rhs.x <<","<<rhs.y<<","<<rhs.z<<"]";
     }
 
     // Overlad Multiplication operator 3D here.
+    //Overload the multiplication operator for our 3D point class 
+    //(just multiply each value with its equivalent in each instance).
+    
+  // anytime asked to overload assignment operator will be done like this
+  // any instance of change, we will change the math symbol to whichever symbol we are using in
+  // function header and the symbol in the body
+  
     Point3D& operator*(const Point3D &rhs) 
     {
         
